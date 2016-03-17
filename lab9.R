5061d70a88b5adb05869052271f9449165a0a436
Jonny <jonny.covert@gmail.com>
2016-03-16 22:03:21 -0400

    Colors are transparent.

--- lab9.R
+++ lab9.R
@@ -1,4 +1,11 @@
 ## These arrays are used for graphing and the legend
+add.alpha <- function(col, alpha=1){
+  if(missing(col))
+      stop("Please provide a vector of colours.")
+  apply(sapply(col, col2rgb)/255, 2, function(x) 
+        rgb(x[1], x[2], x[3], alpha=alpha))  
+}
+
 colors <- c("antiquewhite4",
             "burlywood4",
             "black",
@@ -9,6 +16,8 @@ colors <- c("antiquewhite4",
             "blue",
             "cadetblue3",
             "darkgoldenrod1")
+
+colors <- add.alpha(colors, alpha=0.1)
 
 names <- c("Group A", 
            "Group B", 
@@ -64,13 +73,18 @@ singleGroupGraph <- function (member, connector) {
     xlim <- c(min(up$VoltageIn), max(up$VoltageIn))
     ylim <- c(min(up$VoltageOut), max(up$VoltageOut))
 
-    graph <- plot(x=down$VoltageIn, type = "p", xlim = xlim, ylim = ylim,
-                  xlab = xlab, ylab = ylab,
+    graph <- plot(x=down$VoltageIn, 
+                  y=NULL,
+                  type = "p",
+                  xlim = xlim, 
+                  ylim = ylim,
+                  xlab = xlab,
+                  ylab = ylab,
                   main = "Output Voltage vs Input Voltage")
 
-    lines(y = down$VoltageOut, x = down$VoltageIn, col = "red",
+    lines(y = down$VoltageOut, x = down$VoltageIn, col = colors[1],
           type = "p", pch = 19)
-    lines(y = up$VoltageOut, x = up$VoltageIn, col = "blue",
+    lines(y = up$VoltageOut, x = up$VoltageIn, col = colors[2],
           type = "p", pch = 19)
 
     return("Success!")
